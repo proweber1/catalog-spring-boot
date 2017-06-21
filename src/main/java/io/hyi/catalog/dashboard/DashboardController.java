a0597559f4338e01188505dcccac77a88540135f
Vjachelsav Gusser <proweber1@yandex.ru>
2017-06-21 18:19:22 +0300

    add thymeleaf template engine, home page controller and home page view

---
+++ src/main/java/io/hyi/catalog/dashboard/DashboardController.java
@@ -0,0 +1,13 @@
+package io.hyi.catalog.dashboard;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
